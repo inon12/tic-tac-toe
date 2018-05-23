0c34f8eb70d83c62eeab727112cb3b842783a43b
inon12 <inon.peer12@gmail.com>
2018-05-24 01:14:01 +0300

    update operator=

--- Board.cpp
+++ Board.cpp
@@ -59,6 +59,7 @@ Board& Board::operator=(const char x){
 	return true;
 }*/
 Board& Board::operator=(const Board& B){
+	size=B.size;
 	for(int i=0;i<size;i++)
 		for(int j=0;j<size;j++)
 			this->matrix[i][j]=B.matrix[i][j];
