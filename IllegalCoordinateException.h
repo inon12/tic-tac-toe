67d2702dae885f163fe72de0d93c692ac2d67b25
inonp <inon.peer12@gmail.com>
2022-03-26 20:30:06 +0300

    test commit
    test this commit

--- IllegalCoordinateException.h
+++ IllegalCoordinateException.h
@@ -5,6 +5,7 @@
 using namespace std;
 class IllegalCoordinateException:public exception{
 	public:
+		// testcommit
 		Coordinate coord;
 		IllegalCoordinateException(Coordinate x);
 		string theCoordinate()const ;
